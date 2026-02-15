32d922fe3cb57453a9ec01e811ae39c8279493cb
max <15851628332@163.com>
2026-02-16 00:45:32 +0800

    完成架构与部分内容

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 int main(int /*argc*/, char * /*argv*/[])
 {
-    spdlog::set_level(spdlog::level::info);
+    spdlog::set_level(spdlog::level::off);
     spdlog::info("游戏开始");
 
     engine::core::GameApp app;
